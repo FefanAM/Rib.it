88f630bcd2acbc7721355ec6a63e25eaeec7b814
jaro51 <roskot.j@proton.me>
2025-06-17 18:59:15 +0200

    make diagrams expand when you click on them

--- main.js
+++ main.js
@@ -30,11 +30,41 @@ function iLikeCookies() {
 function selfDestruction() {
     document.body.style.display = "none"
 }
-
 function leftArrow() {
     document.getElementById('onas-container').scrollBy({top:0, left:-1220, behavior: 'smooth'})
 }
 
 function rightArrow() {
     document.getElementById('onas-container').scrollBy({top:0, left:1220, behavior: 'smooth'})
+}
+function openDia() {
+    
+    document.getElementById("obr2").style.opacity = "0.2";
+    document.getElementById("obr3").style.opacity = "0.2";
+    document.getElementById("taskbar").style.opacity = "0.2";
+    document.getElementById("taskbar").style.zIndex= "0";
+    document.getElementById("obr1").style.transform= "translate(85%, -9%)";
+    document.getElementById("obr1").style.zIndex = "4";
+    document.getElementById("obr1").style.height= "98vh";
+    document.getElementById("obr-nav").style.opacity= "1";
+}
+function openDia2() {
+    document.getElementById("obr1").style.opacity = "0.2";
+    document.getElementById("obr3").style.opacity = "0.2";
+    document.getElementById("taskbar").style.opacity = "0.2";
+    document.getElementById("taskbar").style.zIndex= "0";
+    document.getElementById("obr2").style.transform= "translate(0%, -9%)";
+    document.getElementById("obr2").style.zIndex = "4";
+    document.getElementById("obr2").style.height= "98vh";
+    document.getElementById("obr-nav").style.opacity= "1";
+}
+function openDia3() {
+    document.getElementById("obr1").style.opacity = "0.2";
+    document.getElementById("obr2").style.opacity = "0.2";
+    document.getElementById("taskbar").style.opacity = "0.2";
+    document.getElementById("taskbar").style.zIndex= "0";
+    document.getElementById("obr3").style.transform= "translate(-85%, -9%)";
+    document.getElementById("obr3").style.zIndex = "4";
+    document.getElementById("obr3").style.height= "98vh";
+    document.getElementById("obr-nav").style.opacity= "1";
 }
